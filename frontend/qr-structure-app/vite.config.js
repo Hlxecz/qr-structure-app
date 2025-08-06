b1ec2fc8019e70bbab0f49d6084b24f345592c47
HyeungJun <azaz3769@gmail.com>
2025-08-06 15:58:11 +0900

    Update vite.config.js

--- frontend/qr-structure-app/vite.config.js
+++ frontend/qr-structure-app/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'http://3.27.190.251:8080',
         changeOrigin: true,
       },
     },
